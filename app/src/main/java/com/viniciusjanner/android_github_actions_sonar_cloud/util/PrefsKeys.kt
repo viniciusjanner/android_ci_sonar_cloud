1a8cfb5645922f4649b23be20bae788f78f4940d
Vinicius Janner <viniciusjanner@gmail.com>
2023-09-14 23:34:19 -0300

    Add file with constants to DataStore.

---
+++ app/src/main/java/com/viniciusjanner/android_github_actions_sonar_cloud/util/PrefsKeys.kt
@@ -0,0 +1,8 @@
+package com.viniciusjanner.android_github_actions_sonar_cloud.util
+
+object PrefsKeys {
+
+    const val FILE_PREFS_NAME = "FILE_PREFS_NAME"
+
+    const val KEY_DARK_MODE = "KEY_DARK_MODE"
+}
