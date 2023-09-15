cd9673f9e6b5093154bebcafded7674407049771
Vinicius Janner <viniciusjanner@gmail.com>
2023-09-14 23:34:51 -0300

    Reorganized code DataStoreManager.kt.

--- app/src/main/java/com/viniciusjanner/android_github_actions_sonar_cloud/datastore/DataStoreManager.kt
+++ app/src/main/java/com/viniciusjanner/android_github_actions_sonar_cloud/datastore/DataStoreManager.kt
@@ -3,6 +3,8 @@ package com.viniciusjanner.android_github_actions_sonar_cloud.datastore
 import kotlinx.coroutines.flow.Flow
 
 interface DataStoreManager {
+
     suspend fun getTheme(): Flow<Boolean>
+
     suspend fun setTheme(isDarkMode: Boolean)
 }
