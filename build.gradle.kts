9117ca750323160a25183a92a5dec9baab4f919b
Vinicius Janner <viniciusjanner@gmail.com>
2023-09-15 01:49:22 -0300

    Add property sonar exclusions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,6 @@ sonar {
         property("sonar.projectKey", "viniciusjanner_android_github_actions_sonar_cloud")
         property("sonar.organization", "viniciusjanner")
         property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.exclusions", "build.gradle.kts")
     }
 }
